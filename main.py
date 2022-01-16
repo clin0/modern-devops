b79e8dd8e85e488074446d7ab402fec19451aaad
Siyi Lin <44655719+clin0@users.noreply.github.com>
2022-01-16 17:38:29 -0500

    Update main.py

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ app = FastAPI()
 
 @app.get("/")
 def read_root():
-    return {"Hello": "World", "Owner":"Costa Siyi"}
+    return {"Hello": "World", "Owners":"Costa Siyi", "Version":"1"}
 
 
 @app.get("/items/{item_id}")
